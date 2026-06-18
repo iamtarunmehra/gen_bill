faf79fe6ff0445c78e45b65606b3f387c2920419
iamtarunmehra <tarunmehra80790@gmail.com>
2026-06-18 09:12:01 +0530

    ui update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-amber-300 text-black font-bold text-5xl ">
+    <div className="w-full h-screen bg-amber-300 flex items-center justify-center text-black font-bold text-5xl ">
       <h1>Gen Bill | Make Your Business More Structured new </h1>
     </div>
   );
