a1001222e78d284ae0e0330fa9b7cc120071a7ce
iamtarunmehra <tarunmehra80790@gmail.com>
2026-06-18 09:47:58 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-red-500 flex items-center justify-center text-black font-bold text-5xl ">
-      <h1>Gen Bill | Make Your Business More Structured new </h1>
+    <div className="w-full h-screen bg-amber-500 flex items-center justify-center text-black font-bold text-5xl ">
+      <h1>Tarun Mehra </h1>
     </div>
   );
 }
